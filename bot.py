02c11c5a6e1358fcab1c89b9f366eb3dd5835f64
unknown <irajpal2@gmail.com>
2021-02-08 13:24:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,8 @@ from discord import Spotify
 import shutil
 import spotipy
 
-#intents = discord.Intents.default()
-#intents.member = True
-intents = discord.Intents(messages=True, guilds=True)
-
-client = commands.Bot(command_prefix ="")
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='', intents=intents)
 client.remove_command('help')
 
 global name
@@ -21,32 +18,35 @@ global name
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('With my own life'))
-    print("Logged in as: " + client.user.name + "\n")
+    print(f'We have logged in as {client.user}')
+
 @client.event
 async def on_member_join(member):
-    mention=member.mention
     guild=member.guild
-    await member.create_dm()
-    await member.dm_channel.send(str(f"{mention},Welcome to the {guild}").format(mention=mention,guild=guild))
-    print(f'{member} has joined a server.')                    
+    await member.send(str(f'welcome to {guild}'))
+    #await member.send(str(f'welcome to {channel}'))
     for channel in member.guild.channels:
         if str(channel) == "general":
             await channel.send(f"""Welcome to the server {member.mention}""")
-            await channel.send("Member += 1")
+            #await member.dm_channel.send(str(f"{mention},Welcome to the server{guild}").format(mention=mention,guild=guild))
             member_count = len(channel.guild.members)
             await channel.send(f"""Total Members in this Server is: {member_count}""")
-            
+
 @client.event
 async def on_member_remove(member):
     print(f'{member} has left a server.')
     for channel in member.guild.channels:                   #change
         if str(channel) == "general":
-            await channel.send(f"""Member has been Kicked/Removed from server {member.mention}""")
+            await channel.send(f"""Sayonara{member.mention}""")
             await channel.send("Member -= 1")
             member_count = len(channel.guild.members)
             await channel.send(f"""Total Members in this Server is: {member_count}""")
-            
 
+@client.command()
+async def kick(ctx, member : discord.Member, *,reason=None):
+    await ctx.channel.purge(limit=2)
+    await member.kick(reason=reason)
+    await ctx.send(f'kicked{member.mention}')
 @client.command()
 async def ping(ctx):                        #change
     await ctx.send(f"Your ping is :{round(client.latency * 1000)} ms")
@@ -60,14 +60,23 @@ async def avatar(ctx, member: discord.Member):
     show_avatar.set_image(url='{}'.format(member.avatar_url))
     await ctx.send(embed=show_avatar)
 @client.command()
-async def abrakadabra(ctx,amount=5):
+async def clear(ctx,amount=5):
     await ctx.channel.purge(limit=amount)
 
+
 @client.command()
-async def Aaja(ctx):
+async def khela(ctx):
+    await ctx.channel.send("if you want to play you can call me")
+   
+@client.command()
+async def aaja(ctx):
     await ctx.channel.send("if you want to play you can call me")
 
-@client.command(pass_context = True,aliases = ['why','kya'])
+@client.command()
+async def phone(ctx):
+    await ctx.channel.send("if you don't have my Phone no Then don't Bothere")
+
+@client.command()
 async def what(ctx):
     await ctx.channel.send("nothing")
 
@@ -78,7 +87,7 @@ async def users(ctx):
 @client.command()
 async def hi(ctx):
     await ctx.channel.send("hello")
-@client.command(pass_context=True,aliases=['ok', 'so'])
+@client.command()
 async def no(ctx):
     await ctx.channel.send("ok")
 @client.command()
@@ -86,12 +95,12 @@ async def all(ctx):
     await ctx.channel.purge(limit=1)
     await ctx.channel.send("@everyone Khele?")
   
-@client.command(pass_context=True,aliases=['h', 'Help'])
+@client.command()
 async def help(ctx):  
     embed = discord.Embed(title="What can killer Frost do?",description="Some useful commands")
     embed.add_field(name="hi",value="Greets the user")
     embed.add_field(name="users",value="Prints no of users")
-    ##embed.add_field(name="khela",value="message")
+    embed.add_field(name="khela",value="message")
     embed.add_field(name="join",value="add bot to voice channel")
     embed.add_field(name="leave",value="remove bot from voice channel")
     embed.add_field(name="play youtube/Spotify link..",value="play the song")   ##to be 
@@ -101,6 +110,8 @@ async def help(ctx):
     embed.add_field(name="info",value="some basic details")
     embed.add_field(name="all",value="mention everyone to play")
     embed.add_field(name="avatar@user",value="pop up his/her display")
+    
+    
     embed.add_field(name="ping",value="Tells you ping/Current Latency")
     
     await ctx.channel.send(content=None, embed=embed)
@@ -108,10 +119,10 @@ async def help(ctx):
 async def info(ctx):  
     embed = discord.Embed(title="About Killerfrost?",description="Some details")
     embed.add_field(name="Owner",value="Ishan rajpal")
-    embed.add_field(name="Creater Discord I'd",value='Ishan#9884')        ##
+    embed.add_field(name="Creater Discord I'd",value='KillerFrost#9884')        ##
     embed.add_field(name="Main server",value="Hellplay")                        ##to be added
     embed.add_field(name="Capabilities",value="play music and do some stuffs")
-    embed.add_field(name="Instagram",value="https://www.instagram.com/ishan_rajpal/?hl=en")
+    embed.add_field(name="Instagram",value="ishan_rajpal")
     await ctx.channel.send(content=None, embed=embed)
 @client.event
 async def on_command_error(ctx, error):
@@ -120,7 +131,7 @@ async def on_command_error(ctx, error):
 
 @client.command()
 async def chutia(ctx):
-    await ctx.channel.send("Tum chutia")
+    await ctx.channel.send("Tu chutia")
 @client.command(pass_context=True,aliases=['j', 'Join'])
 async def join(ctx):
     global voice
@@ -135,7 +146,7 @@ async def join(ctx):
 
     await ctx.send(f"joined{channel}")
 
-@client.command(pass_context=True, aliases=['l','Leave'])
+@client.command(pass_context=True)
 async def leave(ctx):
     channel = ctx.message.author.voice.channel
     voice = get(client.voice_clients, guild=ctx.guild)
@@ -148,10 +159,10 @@ async def leave(ctx):
         print("not in one")
         await ctx.send("not in one")
 
-@client.command(pass_context=True, aliases=['Play', 'sing','Sing'])
+@client.command(pass_context=True)
 async def play(ctx, url: str):
     
-    song_there = os.environ["song.mp3"]
+    song_there = os.path.isfile("song.mp3")
     try:
         if song_there:
             os.remove("song.mp3")
@@ -215,7 +226,7 @@ async def pause(ctx):
         print("Music not playing failed pause")
         await ctx.send("Music not playing failed pause ")
 
-@client.command(pass_context=True, aliases=['r', 'res','R'])
+@client.command(pass_context=True, aliases=['r', 'res'])
 async def resume(ctx):
     voice = get(client.voice_clients, guild=ctx.guild)
 
