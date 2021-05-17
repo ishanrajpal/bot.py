4153de4d431b6afd90280afc668f6929199c9b12
ishanrajpal <56106026+ishanrajpal@users.noreply.github.com>
2021-05-17 13:34:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,9 +43,6 @@ async def on_member_join(member):
             await channel.send(content=None, embed=embed)
 @client.event
 async def on_member_remove(member):
-    guild=member.guild
-    embed=discord.Embed(title="We will miss you",color=0x9208ea,description=f"{guild}")
-    await member.send(content=None, embed=embed) 
     print(f'{member} has left a server.')
     for channel in member.guild.channels:                   #change
         if str(channel) == "general":
@@ -53,6 +50,9 @@ async def on_member_remove(member):
             await channel.send("Member -= 1")
             member_count = len(channel.guild.members)
             await channel.send(f"""Total Members in this Server is: {member_count}""")
+        guild=member.guild
+        embed=discord.Embed(title="We will miss you",color=0x9208ea,description=f"{guild}")
+        await member.send(content=None, embed=embed) 
 
 #@client.event
 #async def on_member_remove(member):
